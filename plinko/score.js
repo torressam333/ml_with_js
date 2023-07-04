59b4dc1c54be74ed391e41c2606123bceb54ab96
Samuel <torressam333@gmail.com>
2023-07-04 12:02:30 -0700

    Begin recording data per each ball drop in plinko

--- plinko/score.js
+++ plinko/score.js
@@ -1,8 +1,14 @@
+// Main data store for ball drop data
+const outputs = [];
+
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
-  // Ran every time a balls drops into a bucket
+  // Store ball drop data per each drop in 2d-array
+  outputs.push([dropPosition, bounciness, size, bucketLabel]);
+
+  console.log(outputs);
 }
 
 function runAnalysis() {
   // Write code here to analyze stuff
+  console.log('analyzing...');
 }
-
