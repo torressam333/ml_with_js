f4b1d642b710babf789ad93282d89cea0495884c
Samuel <torressam333@gmail.com>
2023-07-05 07:35:31 -0700

    Add sortby and slice to take top k records from outputs

--- plinko/score.js
+++ plinko/score.js
@@ -2,13 +2,18 @@
 const outputs = [];
 // Arbitrary point used for this expirimentation
 const predictionPoint = 300;
+// Arbitrary value for k, can be anything.
+const k = 3;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Store ball drop data per each drop in 2d-array
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
 
   // Map over res and use dropPosition and bucketLabel only
-  _.chain(outputs).map((row) => [distance(row[0]), row[3]]);
+  _.chain(outputs)
+    .map((row) => [distance(row[0]), row[3]])
+    .sortBy((row) => row[0])
+    .slice(0, k); // take top k results
 }
 
 function runAnalysis() {
