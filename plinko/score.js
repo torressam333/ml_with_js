c26c953e4136eaed0fa9a9774ada9775585a0432
Samuel <torressam333@gmail.com>
2023-07-05 07:31:44 -0700

    Use lodash chain to begin filtering and sorting outputs data

--- plinko/score.js
+++ plinko/score.js
@@ -1,14 +1,22 @@
 // Main data store for ball drop data
 const outputs = [];
+// Arbitrary point used for this expirimentation
+const predictionPoint = 300;
 
 function onScoreUpdate(dropPosition, bounciness, size, bucketLabel) {
   // Store ball drop data per each drop in 2d-array
   outputs.push([dropPosition, bounciness, size, bucketLabel]);
 
-  console.log(outputs);
+  // Map over res and use dropPosition and bucketLabel only
+  _.chain(outputs).map((row) => [distance(row[0]), row[3]]);
 }
 
 function runAnalysis() {
   // Write code here to analyze stuff
   console.log('analyzing...');
+}
+
+// Helper fn to do step 1 of knn (sub drop point from 300 as abs value)
+function distance(point) {
+  return Math.abs(point - predictionPoint);
 }
