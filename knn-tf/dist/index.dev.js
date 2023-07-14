e3d418575af9a412ac86854bac261dc2b5e5c128
Samuel <torressam333@gmail.com>
2023-07-14 07:07:17 -0700

    Add arraySync method to get correct output in logs

--- knn-tf/dist/index.dev.js
+++ knn-tf/dist/index.dev.js
@@ -1,10 +1,15 @@
 "use strict";
 
-var tf = require('@tensorflow/tfjs-node');
+var tf = require('@tensorflow/tfjs-node'); // Example 1
+
 
 var data = tf.tensor([1, 2, 3]);
 var otherData = tf.tensor([4, 5, 6]);
-data.div(otherData); // console.log('shape:', data.shape);
-// console.log('Sub:', data.sub(otherData));
+var result = data.div(otherData);
+console.log(result.arraySync()); // Convert tensor to a JavaScript array [ 0.25, 0.4000000059604645, 0.5 ]
+// Example 2
 
-console.log('Div:', data);
+var data2 = tf.tensor([1, 2, 3]);
+var otherData2 = tf.tensor([4]);
+var result2 = data2.add(otherData2);
+console.log(result2.arraySync()); // [5,6,7]
--- knn-tf/index.js
+++ knn-tf/index.js
@@ -1,11 +1,16 @@
 const tf = require('@tensorflow/tfjs-node');
 
+// Example 1
 const data = tf.tensor([1, 2, 3]);
-
 const otherData = tf.tensor([4, 5, 6]);
 
-data.div(otherData);
+const result = data.div(otherData);
+console.log(result.arraySync()); // Convert tensor to a JavaScript array [ 0.25, 0.4000000059604645, 0.5 ]
 
-// console.log('shape:', data.shape);
-// console.log('Sub:', data.sub(otherData));
-console.log('Div:', data);
+// Example 2
+const data2 = tf.tensor([1, 2, 3]);
+const otherData2 = tf.tensor([4]);
+
+const result2 = data2.add(otherData2);
+
+console.log(result2.arraySync()); // [5,6,7]
