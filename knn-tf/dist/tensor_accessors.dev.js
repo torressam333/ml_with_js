64b7a141c0228fd71b29740b433c36133839f9c5
Samuel <torressam333@gmail.com>
2023-07-16 08:46:06 -0700

    Add example of how to get single column of values of tesnor

--- knn-tf/dist/tensor_accessors.dev.js
+++ knn-tf/dist/tensor_accessors.dev.js
@@ -2,9 +2,14 @@
 
 var tf = require('@tensorflow/tfjs-node');
 
-var data = tf.tensor([[10, 20, 30], [100, 200, 300]]);
+var data = tf.tensor([[10, 20, 30], [100, 200, 300], [100, 40, 300], [100, 50, 300], [100, 60, 300], [100, 70, 300], [100, 80, 300]]);
 var data2 = tf.tensor([10, 20, 30]); // Access single element
 
 console.log(data.arraySync()[1][2]); // 300
 
 console.log(data2.arraySync()[1]); // 20
+// Access many elements in a tensor
+// Grab only 2nd col from data
+
+var secondCol = data.slice([0, 1], [7, 1]);
+console.log(secondCol.arraySync());
--- knn-tf/tensor_accessors.js
+++ knn-tf/tensor_accessors.js
@@ -3,6 +3,11 @@ const tf = require('@tensorflow/tfjs-node');
 const data = tf.tensor([
   [10, 20, 30],
   [100, 200, 300],
+  [100, 40, 300],
+  [100, 50, 300],
+  [100, 60, 300],
+  [100, 70, 300],
+  [100, 80, 300],
 ]);
 
 const data2 = tf.tensor([10, 20, 30]);
@@ -10,3 +15,9 @@ const data2 = tf.tensor([10, 20, 30]);
 // Access single element
 console.log(data.arraySync()[1][2]); // 300
 console.log(data2.arraySync()[1]); // 20
+
+// Access many elements in a tensor
+
+// Grab only 2nd col from data
+const secondCol = data.slice([0, 1], [7, 1]);
+console.log(secondCol.arraySync());
