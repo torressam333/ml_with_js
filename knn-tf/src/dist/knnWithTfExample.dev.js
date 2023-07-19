e40716ad596854cbe1b5ad531c1add7c30eaf62a
Samuel <torressam333@gmail.com>
2023-07-19 07:00:46 -0700

    Create initial housing data for knn with tf

--- knn-tf/src/dist/knnWithTfExample.dev.js
+++ knn-tf/src/dist/knnWithTfExample.dev.js
@@ -1,3 +1,13 @@
 "use strict";
 
 var tf = require('@tensorflow/tfjs-node');
+/**
+ * Features order and labels order are in sync ([-124,47]) will
+ * line up with the label of [200] etc...
+ */
+// Fake sample data for housing info (long/lat for houses)
+
+
+var features = tf.tensor([[-124, 47], [-121.2, 46.5], [-122, 46.4], [-120, 46.7]]); // Collection of labels (property values for houses)
+
+var labels = tf.tensor([[200], [250], [215], [240]]);
--- knn-tf/src/knnWithTfExample.js
+++ knn-tf/src/knnWithTfExample.js
@@ -1,1 +1,16 @@
 const tf = require('@tensorflow/tfjs-node');
+
+/**
+ * Features order and labels order are in sync ([-124,47]) will
+ * line up with the label of [200] etc...
+ */
+// Fake sample data for housing info (long/lat for houses)
+const features = tf.tensor([
+  [-124, 47],
+  [-121.2, 46.5],
+  [-122, 46.4],
+  [-120, 46.7],
+]);
+
+// Collection of labels (property values for houses)
+const labels = tf.tensor([[200], [250], [215], [240]]);
