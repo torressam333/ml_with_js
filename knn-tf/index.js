00ebce11cc1ea77ad63e3ffa8a51b0dac8f79083
Samuel <torressam333@gmail.com>
2023-08-01 07:11:59 -0700

    Pass needed properties to loadCsv functions

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -2,4 +2,17 @@ require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfs');
 const loadCSV = require('./load-csv');
 
-loadCSV('kc_house_data.csv', {});
+// Returns object
+let { features, labels, testFeatures, testLabels } = loadCSV(
+  'kc_house_data.csv',
+  {
+    // Shuffle rows of data in csv
+    shuffle: true,
+    // #of records creating 2 sets of data (1. Training 2. Testing) purposes
+    splitTest: 10,
+    // Which cols from data to test against
+    dataColumns: ['lat', 'long'],
+    // Which col we want as our label (i.e. using lat + long to determine price)
+    labelColumns: ['price'],
+  }
+);
